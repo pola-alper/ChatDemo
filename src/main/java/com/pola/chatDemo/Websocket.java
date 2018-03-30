44620e3f04bd4471f57c054d29843b81cac399f8
pola-alper <pola.alper@gmail.com>
2018-03-30 17:26:46 +0530

    Initial commit

---
+++ src/main/java/com/pola/chatDemo/Websocket.java
@@ -0,0 +1,12 @@
+package com.pola.chatDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket.class, args);
+	}
+}
